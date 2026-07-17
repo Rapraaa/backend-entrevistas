7668f7614714850df748a05a24f9093d79bbba4d
LaJoseth2005 <josethcarrera2005@gmail.com>
2026-07-17 16:44:29 -0500

    feat: implementacion de homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SetupPage } from './pages/SetupPage';
 import { InterviewPage } from './pages/InterviewPage';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SetupPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/setup" element={<SetupPage />} />
         <Route path="/interview" element={<InterviewPage />} />
       </Routes>
     </BrowserRouter>
