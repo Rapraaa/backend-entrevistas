fd0fcc0697a15822876c5a29a5143e2faef56b99
LaJoseth2005 <josethcarrera2005@gmail.com>
2026-07-18 16:34:04 -0500

    Front registro (#23)
    * feat: implementacion de homepage
    
    * feat: implementacion de login
    
    * feat: implementacion de registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SetupPage } from './pages/SetupPage';
 import { InterviewPage } from './pages/InterviewPage';
+import { Home } from './pages/Home';
+import { Registro } from './pages/Registro';
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SetupPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/setup" element={<SetupPage />} />
         <Route path="/interview" element={<InterviewPage />} />
       </Routes>
     </BrowserRouter>
