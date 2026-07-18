0116c208aafba20da298615a9831910000c39309
Rapraaa <rahiruilova33@gmail.com>
2026-07-18 17:25:07 -0500

    feat: rutas con parámetros y flujo de navegación completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProtectedRoute } from './auth/ProtectedRoute';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Registro } from './pages/Registro';
+import { DashboardPage } from './pages/DashboardPage';
 import { SetupPage } from './pages/SetupPage';
 import { InterviewPage } from './pages/InterviewPage';
-import { DashboardPage } from './pages/DashboardPage';
 import { ResultPage } from './pages/ResultPage';
-import { Home } from './pages/Home';
-import { Registro } from './pages/Registro';
-import { Login } from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/setup" element={<SetupPage />} />
-        <Route path="/interview" element={<InterviewPage />} />
-        <Route path="/resultado" element={<ResultPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/setup"
+          element={
+            <ProtectedRoute>
+              <SetupPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/interview/:id"
+          element={
+            <ProtectedRoute>
+              <InterviewPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/resultado/:id"
+          element={
+            <ProtectedRoute>
+              <ResultPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
