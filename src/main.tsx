28c070538b0a4670c6369bc797234fc0874d9bf5
Rapraaa <rahiruilova33@gmail.com>
2026-07-18 18:29:08 -0500

    feat: layout privado con sidebar, footer, breadcrumbs y notificaciones

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './auth/AuthContext.tsx'
+import { NotificationProvider } from './contexts/NotificationContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </AuthProvider>
   </StrictMode>,
 )
